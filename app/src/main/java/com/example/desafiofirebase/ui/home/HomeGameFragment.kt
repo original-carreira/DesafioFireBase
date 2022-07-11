7c8bf7d6ea60c403756057bc90d34f6471ce6309
Victor Carreira <sg.el.carreira@hotmail.com>
2022-07-11 00:52:09 -0300

    Codificada a navegação e acerto no xml de fragments

---
+++ app/src/main/java/com/example/desafiofirebase/ui/home/HomeGameFragment.kt
@@ -0,0 +1,32 @@
+package com.example.desafiofirebase.ui.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.desafiofirebase.R
+
+class HomeGameFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeGameFragment()
+    }
+
+    private lateinit var viewModel: HomeGameViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_game, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeGameViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/desafiofirebase/ui/home/HomeGameViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.desafiofirebase.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeGameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
