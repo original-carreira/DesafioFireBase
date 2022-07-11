a0a0d9db99b7c7cfcb1098a623f7e26511720862
Victor Carreira <sg.el.carreira@hotmail.com>
2022-07-11 01:25:20 -0300

    Codificada AddGame e acerto no xml de fragments

--- app/src/main/java/com/example/desafiofirebase/ui/home/CadastroGameFragment.kt
+++ app/src/main/java/com/example/desafiofirebase/ui/home/CadastroGameFragment.kt
@@ -1,11 +1,13 @@
 package com.example.desafiofirebase.ui.home
 
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.desafiofirebase.R
 import com.example.desafiofirebase.databinding.FragmentCadastroGameBinding
 
@@ -13,6 +15,10 @@ class CadastroGameFragment : Fragment() {
 
    private var _binding: FragmentCadastroGameBinding? = null
     private val binding get() = _binding!!
+    private var imageUri: Uri? = null
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){
+        uri -> imageUri = uri
+    }
     private lateinit var viewModel: CadastroGameViewModel
 
     override fun onCreateView(
@@ -21,6 +27,19 @@ class CadastroGameFragment : Fragment() {
     ): View {
         _binding = FragmentCadastroGameBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    private fun setListeners(){
+        binding.botaoFlutuanteAddGame.setOnClickListener {
+
+        }
+
+        binding.buttonSavedGame.setOnClickListener {
+            val nomeGame = binding.nameCreateEditText.text.toString()
+            val dataGame = binding.dataAddGameEditText.text.toString()
+            val descriptionGame = binding.descriptionAddGameEditText.text.toString()
+        }
+
     }
 
 }
